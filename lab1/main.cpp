d128834c7198e11125c0dbe495f7bbcf4141bc52
german <grmnandreev@gmail.com>
2025-02-13 10:20:16 +0300

    add.lab1

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
-#include <string.h>
+#include "mymath/mymath.h"
 
-int interactive();
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Я умею суммировать! Зацени 1+1=";
+    std::cout << mymath::sum(10, 32) << std::endl;
 
-int demo();
-
-int main(int argc, char *argv[]) {
-    bool isInteractive = false;    // по умолчанию демо-режим.
-
-    // Если i передается в качестве аргумента, то программу
-    // необходимо запустить в интерактивном режиме
-    if ((argc == 2) && strcmp(argv[1], "i") == 0) {
-        isInteractive = true;
-    }
-
-    if (isInteractive) {
-        return interactive();
-
-    } else {
-        return demo();
-    }
+    return 0;
 }
