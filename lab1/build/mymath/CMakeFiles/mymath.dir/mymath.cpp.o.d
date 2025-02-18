70d2ed3cdd892534746813eb59f94fed5374fec0
german <grmnandreev@gmail.com>
2025-02-18 12:37:59 +0300

    update.lab1

---
+++ lab1/build/mymath/CMakeFiles/mymath.dir/mymath.cpp.o.d
@@ -0,0 +1,4 @@
+mymath/CMakeFiles/mymath.dir/mymath.cpp.o: \
+ /home/oem/labs/Andreev-Sem02/lab1/mymath/mymath.cpp \
+ /usr/include/stdc-predef.h \
+ /home/oem/labs/Andreev-Sem02/lab1/mymath/mymath.h
