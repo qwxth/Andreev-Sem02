0de5ce1a49e8396b333c6e46739aeae834f03d1e
german <grmnandreev@gmail.com>
2025-03-04 12:43:59 +0300

    update.lab2

--- lab2/Bus/Bus.cpp
+++ lab2/Bus/Bus.cpp
@@ -128,7 +128,15 @@ void Bus::writeToFile(const char* filename, const Bus* buses, int size) {
 }
 
 void Bus::sortBuses(Bus* buses, int size) {
-    std::sort(buses, buses + size);
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (buses[j + 1] < buses[j]) {
+                Bus temp = buses[j];
+                buses[j] = buses[j + 1];
+                buses[j + 1] = temp;
+            }
+        }
+    }
 }
 
 void Bus::addBus(Bus*& buses, int& size, const Bus& newBus) {
