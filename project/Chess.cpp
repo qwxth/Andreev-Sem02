0b0c4222501e2e3bfe276339f8f87e79479ca792
balvan4ik-228 <home-andru@mail.ru>
2025-04-01 14:34:22 +0300

    52

--- project/Chess.cpp
+++ project/Chess.cpp
@@ -7,27 +7,7 @@
 #include <vector>
 #include <iomanip>
 
-void Print(std::vector<std::vector<std::string>>& board, const std::string& move = "") {
-    // Обработка хода
-    if (!move.empty() && move.size() == 5 && move[2] == '-') {
-        std::string from = move.substr(0, 2);
-        std::string to = move.substr(3, 2);
-
-        // Конвертация координат
-        int from_col = std::tolower(from[0]) - 'a';
-        int from_row = 8 - (from[1] - '0');
-        int to_col = std::tolower(to[0]) - 'a';
-        int to_row = 8 - (to[1] - '0');
-
-        // Проверка границ и выполнение хода
-        if (from_row >= 0 && from_row < 8 && from_col >= 0 && from_col < 8 &&
-            to_row >= 0 && to_row < 8 && to_col >= 0 && to_col < 8) {
-            board[to_row][to_col] = board[from_row][from_col];
-            board[from_row][from_col] = " ";
-        }
-    }
-
-    // Отображение доски
+void Print(std::vector<std::vector<std::string>>& board) {
     std::cout << "    a    b    c    d    e    f    g    h\n";
     std::cout << "  +----+----+----+----+----+----+----+----+\n";
 
@@ -42,35 +22,74 @@ void Print(std::vector<std::vector<std::string>>& board, const std::string& move
     std::cout << "    a    b    c    d    e    f    g    h\n";
 }
 
+void Move(std::vector<std::vector<std::string>>& board, const std::string& move = ""){
+    if (!move.empty() && move.size() == 5 && move[2] == '-') {
+        std::string from = move.substr(0, 2);
+        std::string to = move.substr(3, 2);
+
+        int fromCol = std::tolower(from[0]) - 'a';
+        int fromRow = 8 - (from[1] - '0');
+        int toCol = std::tolower(to[0]) - 'a';
+        int toRow = 8 - (to[1] - '0');
+
+        if (fromRow >= 0 && fromRow < 8 && fromCol >= 0 && fromCol < 8 &&
+            toRow >= 0 && toRow < 8 && toCol >= 0 && toCol < 8) {
+            board[toRow][toCol] = board[fromRow][fromCol];
+            board[fromRow][fromCol] = " ";
+        }
+    }
+}
+
 void FirstPlayerStep(){}
 
 void SecondPlayerStep(){}
 
-void StartApp(bool gameGoing) {
-    while (gameGoing == true) {
-        FirstPlayerStep();
-        SecondPlayerStep();
-
+bool KingCheck(std::vector<std::vector<std::string>>& board){
+    int countKing = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j<8; j++) {
+            if (board[i][j] == " ♚ ") {
+                countKing += 1;
+            }
+        }
     }
+    return countKing != 2;
+}
+
+int GameEnding(std::vector<std::vector<std::string>>& board) {
+    if (KingCheck(board)) {
+        return 1;
+    }
+    return 0;
+}
+
+void StartApp(std::vector<std::vector<std::string>>& board) {
+    bool GameContinues = true;
+    while(GameContinues) {
+        Print(board);
+
+        std::string move;
+        std::cin >> move;
+
+        Move(board, move);
+
+        if (GameEnding(board) == 1) {
+            GameContinues = false;
+        }
+    }
+    std::cout << "Игра конченая" << std::endl;
 }
 
 int main() {
     std::vector<std::vector<std::string>> board(8, std::vector<std::string>(8, " "));
 
-    // Белые фигуры
     board[0] = {" ♜ ", " ♞ ", " ♝ ", " ♛ ", " ♚ ", " ♝ ", " ♞ ", " ♜ "};
     board[1] = {" ♟ ", " ♟ ", " ♟ ", " ♟ ", " ♟ ", " ♟ ", " ♟ ", " ♟ "};
 
-    // Черные фигуры
     board[7] = {" ♜ ", " ♞ ", " ♝ ", " ♛ ", " ♚ ", " ♝ ", " ♞ ", " ♜ "};
     board[6] = {" ♟ ", " ♟ ", " ♟ ", " ♟ ", " ♟ ", " ♟ ", " ♟ ", " ♟ "};
 
-    Print(board);
-    while(true) {
-        std::string move;
-        std::cin >> move;
+    StartApp(board);
 
-        Print(board, move);
-    }
     return 0;
 }
